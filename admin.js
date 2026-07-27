3897e9431c4f449313fb7aad228a865a0b742d8a
Rana Rauf <raufrana0322>
2026-07-27 15:47:10 +0500

    Campus Lost & Found

---
+++ admin.js
@@ -0,0 +1,7 @@
+// ================================================================
+//  Campus Lost & Found — admin.js  (ADMIN PANEL)
+//
+//  HTML and CSS are ready. Build the JavaScript here.
+//  Open DevTools Console (F12) before you start.
+//  Make sure JSON Server is running: json-server --watch db.json
+// ================================================================
---
+++ app.js
@@ -0,0 +1,10 @@
+// ================================================================
+//  Campus Lost & Found — app.js  (USER PANEL)
+//
+//  HTML and CSS are ready. Build the JavaScript here.
+//  Open DevTools Console (F12) before you start.
+//  Make sure JSON Server is running: json-server --watch db.json
+// ================================================================
+const API_URL =" https://localhost:3000/reports";
+const StatusmsgEl  = document.getElement By Id ("status-msg");
+const reportList = document.getElement ById("report-list");
